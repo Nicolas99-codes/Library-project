909850402ce8352abf92cc0c15178d8e3b5d6d62
Nicolas <NicolasxFranck@gmail.com>
2023-06-18 10:59:30 +0200

    Price added

--- src/main/java/nicolas/library/model/Book.java
+++ src/main/java/nicolas/library/model/Book.java
@@ -26,6 +26,8 @@ public class Book {
 
     private String release_year;
 
+    private String price;
+
 
     @ManyToMany
     private Collection<Author> authors;
@@ -39,7 +41,7 @@ public class Book {
     public Book() {
     }
 
-    public Book(Integer id, String title, String author, String genre, String category, String description, String status, String release_year) {
+    public Book(Integer id, String title, String author, String genre, String category, String description, String status, String release_year, String price) {
         this.id = id;
         this.title = title;
         this.author = author;
@@ -48,6 +50,7 @@ public class Book {
         this.description = description;
         this.status = status;
         this.release_year = release_year;
+        this.price = price;
     }
 
     public Integer getId() {
@@ -112,6 +115,14 @@ public class Book {
 
     public void setRelease_year(String release_year) {
         this.release_year = release_year;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
     }
 
     public Collection<Author> getAuthors() {
