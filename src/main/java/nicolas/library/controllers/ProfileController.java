907335f1333bca31c4416633b6c302a0e018dfb7
Nicolas <NicolasxFranck@gmail.com>
2024-06-14 17:21:12 +0200

    UI for profile

--- src/main/java/nicolas/library/controllers/ProfileController.java
+++ src/main/java/nicolas/library/controllers/ProfileController.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 import java.util.Optional;
@@ -49,5 +50,45 @@ public class ProfileController {
             }
         }
         return "profilePage";
+    }
+
+    @GetMapping("/gegevensPagina")
+    public String gegevensPagina(Model model, Principal principal){
+        Optional<AppUser> optionalAppUser = Optional.empty();
+        if(principal != null){
+            optionalAppUser = userRepository.findByUsername(principal.getName());
+            if (optionalAppUser.isPresent()) {
+                model.addAttribute("appUser", optionalAppUser.get());
+            }
+        }
+        return "dataPage";
+    }
+
+    @PostMapping("/gegevensPaginaWijzigen")
+    public String gegevensPaginaWijzigen(String firstName, String surname, String email, String currentPassword, String newPassword, Principal principal){
+        Optional<AppUser> optionalAppUser = Optional.empty();
+        if(principal != null){
+            optionalAppUser = userRepository.findByUsername(principal.getName());
+            if (optionalAppUser.isPresent()){
+                AppUser appUserToUpdate = optionalAppUser.get();
+
+                if (!currentPassword.isEmpty() && !newPassword.isEmpty()) {
+                    if (passwordEncoder.matches(currentPassword, appUserToUpdate.getPassword())) {
+                        return "redirect:/profielPagina";
+                    }
+
+                    String encodedNewPassword = passwordEncoder.encode(newPassword);
+                    appUserToUpdate.setPassword(encodedNewPassword);
+                }
+
+                appUserToUpdate.setFirstName(firstName);
+                appUserToUpdate.setSurname(surname);
+                appUserToUpdate.setEmail(email);
+
+                userRepository.save(appUserToUpdate);
+            }
+        }
+
+        return "redirect:/profielPagina";
     }
 }
